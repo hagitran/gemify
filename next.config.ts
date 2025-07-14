6de13bdfb23308554e791a364097d1cdd9f078ea
hagi <116292851+hagitran@users.noreply.github.com>
2025-07-14 13:49:48 +0700

    fix images on deployed

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
-
+module.exports = {
+  images: {
+    domains: [
+      "ktulfgdnuooptgufjqcf.supabase.co", // <-- add your Supabase project domain here
+    ],
+  },
+};
 export default nextConfig;

c5bb76a12decb4550fa1eb779a30485a7900ac28
hagi <116292851+hagitran@users.noreply.github.com>
2025-07-16 14:11:01 +0700

    push vercel geocode fix

--- src/app/api/geo/route.ts
+++ src/app/api/geo/route.ts
@@ -1,8 +1,9 @@
 import { geolocation } from "@vercel/functions";
 
 export async function GET(request: Request) {
-  const data = geolocation(request);
-  console.log(data, "data");
-  //   console.log(city, "sio");
-  return Response.json({ city: data.city?.toLowerCase() || "hcmc" });
+  const { city } = geolocation(request);
+
+  const shown = city == "ho chi minh city" ? "hcmc" : "sf";
+
+  return Response.json({ city: shown });
 }
